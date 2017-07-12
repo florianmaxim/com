ab91e8ec3e52f3704d76f3dfedfea17703a2a0c2
Florian Maxim <look@cheesyeyes.com>
2017-07-12 09:55:38 +0200

    added routes

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var express = require('express')
 
+var path = require('path');
+
 var app = express();
 
 var _PORT = 5080;
 
-app.use(express.static('build'));
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, 'build')));
 
-app.listen(_PORT, function () {
-  console.log('Listening on port '+_PORT+'!');
+// Always return the main index.html, so react-router renders the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+app.listen(_PORT, () => {
+  console.log(`App listening on port ${_PORT}!`);
 });
